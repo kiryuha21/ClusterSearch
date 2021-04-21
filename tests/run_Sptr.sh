53692e6880333ca579f81d8798e112bf508dbcc0
Кирилл <kiryuha_21@mail.ru>
2021-04-21 22:00:26 +0300

    pipeline upgraded for Forel algorithm

---
+++ tests/run_Sptr.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+PROG_PATH=${1}
+
+IN1="
+manually
+1
+1
+100
+0
+100
+0
+100
+7
+2
+2
+main_output.txt
+-1
+2
+3
+3
+6
+"
+
+IN2="
+file
+main_output.txt
+-1
+2
+3
+3
+6
+"
+
+echo "${IN1}" | eval "${PROG_PATH}"
+
+echo "${IN2}" | eval "${PROG_PATH}"
